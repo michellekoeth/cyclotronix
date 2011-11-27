605eb78f4f81c966857646a0fa7f09fd86beb094
Michelle Koeth <michelle@codeforamerica.org>
2011-11-26 17:59:11 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyclotronix::Application.config.secret_token = '0c44648684f44d89285a51b377e71686c1714c7330435743fe7279975e73ce432514a43e06ce066bb84ea21ac4bfd640e217260a18ebce2910edb6009319e7b5'
